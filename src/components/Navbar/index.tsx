d7b465f1c64a69d3bc60da6928176b24e434c7f2
rajdip-b <agentR47@gmail.com>
2023-08-07 22:20:38 +0530

    feat+patch: Add blog link and made optimization

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -11,12 +11,16 @@ import DarkModeToggler from '@/components/Navbar/DarkModeToggler';
 
 type Props = {
 	onToggleMode: () => void;
-}
+};
 
 const links = [
 	{
 		name: 'Home',
 		href: '#home',
+	},
+	{
+		name: 'Blog',
+		href: socials.blog,
 	},
 	{
 		name: 'Skills',
@@ -47,51 +51,85 @@ const Navbar: React.FC<Props> = ({ onToggleMode }) => {
 	return (
 		<>
 			<div
-				className={'w-screen px-5 md:px-10 lg:px-20 absolute bg-light-primary dark:bg-dark-primary py-10 justify-between text-dark-primary dark:text-light-primary md:flex hidden'}>
+				className={
+					'w-screen px-5 md:px-10 lg:px-20 absolute bg-light-primary dark:bg-dark-primary py-10 justify-between text-dark-primary dark:text-light-primary md:flex hidden'
+				}
+			>
 				<span className={`${orionPrime.className}`}>R.B</span>
 				<div className={'flex gap-x-8'}>
-					{
-						links.map((link, index) => (
-							<Link
-								scroll={false}
-								className={`${router.asPath.split('/')[1] === link.href ? 'text-primary' : 'text-dark-primary dark:text-light-primary'} transition-all ease-out duration-300`}
-								href={link.href} key={index}>{link.name}</Link>
-						))
-					}
+					{links.map((link, index) => (
+						<Link
+							scroll={false}
+							className={`${
+								router.asPath.split('/')[1] === link.href
+									? 'text-primary'
+									: 'text-dark-primary dark:text-light-primary'
+							} transition-all ease-out duration-300`}
+							href={link.href}
+							key={index}
+						>
+							{link.name}
+						</Link>
+					))}
 				</div>
 				<div className={'flex flex-row gap-x-5 items-center'}>
 					<DarkModeToggler onToggleMode={onToggleMode} />
 					<div className={'flex gap-x-4'}>
-						<a href={socials.linkedin}><LinkedIn /></a>
-						<a href={socials.github}><GitHub /></a>
-						<a href={socials.facebook}><Facebook /></a>
+						<a href={socials.linkedin}>
+							<LinkedIn />
+						</a>
+						<a href={socials.github}>
+							<GitHub />
+						</a>
+						<a href={socials.facebook}>
+							<Facebook />
+						</a>
 					</div>
 				</div>
 			</div>
 			<div
-				className={`z-10 flex md:hidden flex-col absolute text-dark-primary dark:text-light-primary w-screen ${expanded ? 'h-[340px] drop-shadow-xl bg-light-secondary dark:bg-dark-secondary' : 'h-[100px] bg-light-primary dark:bg-dark-primary'} overflow-hidden transition-all ease-in-out duration-300`}>
+				className={`z-10 flex md:hidden flex-col absolute text-dark-primary dark:text-light-primary w-screen ${
+					expanded
+						? 'h-[380px] drop-shadow-xl bg-light-secondary dark:bg-dark-secondary'
+						: 'h-[100px] bg-light-primary dark:bg-dark-primary'
+				} overflow-hidden transition-all ease-in-out duration-300`}
+			>
 				<div className={'px-5 py-10 flex justify-between items-center'}>
 					<span className={`${orionPrime.className}`}>R.B</span>
 					<div className={'flex gap-x-5'}>
 						<DarkModeToggler onToggleMode={onToggleMode} />
-						<button onClick={() => setExpanded(prev => !prev)}><MenuRounded /></button>
+						<button onClick={() => setExpanded(prev => !prev)}>
+							<MenuRounded />
+						</button>
 					</div>
 				</div>
 				<div className={'flex flex-col gap-y-5 items-center'}>
 					<div className={'flex flex-col gap-y-2 text-center'}>
-						{
-							links.map((link, index) => (
-								<Link
-									scroll={false}
-									className={`${router.asPath.split('/')[1] === link.href ? 'text-yellow-light dark:text-yellow-dark' : 'text-dark-primary dark:text-light-primary'} transition-all ease-out duration-300`}
-									href={link.href} key={index}>{link.name}</Link>
-							))
-						}
+						{links.map((link, index) => (
+							<Link
+								scroll={false}
+								className={`${
+									router.asPath.split('/')[1] === link.href
+										? 'text-yellow-light dark:text-yellow-dark'
+										: 'text-dark-primary dark:text-light-primary'
+								} transition-all ease-out duration-300`}
+								href={link.href}
+								key={index}
+							>
+								{link.name}
+							</Link>
+						))}
 					</div>
 					<div className={'gap-x-4 flex'}>
-						<a href={socials.linkedin}><LinkedIn /></a>
-						<a href={socials.github}><GitHub /></a>
-						<a href={socials.facebook}><Facebook /></a>
+						<a href={socials.linkedin}>
+							<LinkedIn />
+						</a>
+						<a href={socials.github}>
+							<GitHub />
+						</a>
+						<a href={socials.facebook}>
+							<Facebook />
+						</a>
 					</div>
 				</div>
 			</div>
--- src/components/Skillset/SkillStack.tsx
+++ src/components/Skillset/SkillStack.tsx
@@ -6,26 +6,28 @@ import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 type Props = {
 	stack: TSkillStack;
-}
+};
 
 const SkillStack: React.FC<Props> = ({ stack }) => {
 	return (
 		<div
-			className={`rounded-lg bg-light-primary dark:bg-dark-primary h-full py-10 px-16 w-full items-center justify-start flex flex-col gap-y-5 hover:drop-shadow-2xl drop-shadow transition-all ease-out duration-300`}>
-			<Image width={70} height={70} src={require(`../../assets/images/${stack.logo}`)}
-				   alt={stack.logo.split('.')[0]} />
+			className={`rounded-lg bg-light-primary dark:bg-dark-primary h-full py-10 px-16 w-full items-center justify-start flex flex-col gap-y-5 hover:drop-shadow-2xl drop-shadow transition-all ease-out duration-300`}
+		>
+			<Image
+				priority={true}
+				width={70}
+				height={70}
+				src={require(`../../assets/images/${stack.logo}`)}
+				alt={stack.logo.split('.')[0]}
+			/>
 			<div className={'text-xl font-semibold text-center'}>{stack.title}</div>
 			<div className={'text-center'}>{stack.subtitle}</div>
-			<div
-				className={'flex gap-3 cursor-pointer my-5 w-full flex-wrap items-center justify-center'}>
-				{
-					stack.skills.map((skill, index) => (
-						<AnimationOnScroll key={index} animateIn={'animate__fadeInRight'} animateOnce
-										   delay={index * 200}>
-							<Skill skill={skill} />
-						</AnimationOnScroll>
-					))
-				}
+			<div className={'flex gap-3 cursor-pointer my-5 w-full flex-wrap items-center justify-center'}>
+				{stack.skills.map((skill, index) => (
+					<AnimationOnScroll key={index} animateIn={'animate__fadeInRight'} animateOnce delay={index * 200}>
+						<Skill skill={skill} />
+					</AnimationOnScroll>
+				))}
 			</div>
 		</div>
 	);
