8735870e1ece758a771ce3674e0e780ad9637d86
Rajdip Bhattacharya <agentR47@gmail.com>
2023-08-10 10:25:02 +0530

    patch: Update socials.ts

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,7 +2,7 @@ const socials = {
 	github: 'https://github.com/rajdip-b',
 	linkedin: 'https://www.linkedin.com/in/rajdip-bhattacharya-581119232/',
 	facebook: 'https://www.facebook.com/rajdip.bhattacharya.334/',
-	blog: 'https://blog.rajdip.dev/',
+	blog: 'https://dev.to/thecodersden',
 };
 
 export default socials;
