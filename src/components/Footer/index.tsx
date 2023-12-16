4fb9c5eaea3041b39722e6bfbc46a4212f955dbe
rajdip-b <agentR47@gmail.com>
2023-12-16 11:08:54 +0530

    feat: add twitter in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,6 +5,7 @@ import GitHub from '@mui/icons-material/GitHub';
 import Facebook from '@mui/icons-material/Facebook';
 import socials from '@/data/socials';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
+import { Twitter } from '@mui/icons-material';
 
 const orionPrime = localFont({
 	src: '../../assets/fonts/Orionprimedemo.ttf',
@@ -12,8 +13,13 @@ const orionPrime = localFont({
 
 const Footer = () => {
 	return (
-		<AnimationOnScroll animateIn={'animate__fadeIn'} animateOnce={true}
-						   className={'w-screen py-16 bg-light-secondary dark:bg-dark-secondary text-dark-primary dark:text-light-primary'}>
+		<AnimationOnScroll
+			animateIn={'animate__fadeIn'}
+			animateOnce={true}
+			className={
+				'w-screen py-16 bg-light-secondary dark:bg-dark-secondary text-dark-primary dark:text-light-primary'
+			}
+		>
 			<div className={'flex items-center gap-x-3 px-5 md:px-10 lg:px-20'}>
 				<span className={`${orionPrime.className} text-3xl`}>R.B</span>
 				<div className={'flex flex-col dark:text-gray-400 text-gray-600'}>
@@ -22,26 +28,46 @@ const Footer = () => {
 				</div>
 			</div>
 			<div
-				className={'border-y-2 dark:border-y-gray-300 border-y-gray-400 w-screen px-5 md:px-10 lg:px-20 py-10 flex md:flex-row flex-col gap-y-10 justify-between mt-5'}>
+				className={
+					'border-y-2 dark:border-y-gray-300 border-y-gray-400 w-screen px-5 md:px-10 lg:px-20 py-10 flex md:flex-row flex-col gap-y-10 justify-between mt-5'
+				}
+			>
 				<div className={'flex flex-col gap-y-5 md:w-1/5'}>
-					<div className={'dark:text-gray-400 text-gray-600 text-sm'}>Rajdip Bhattacharya is Full Stack
-						developer, Cloud
-						and
-						DevOps
-						engineer based in Kolkata,
-						India.
+					<div className={'dark:text-gray-400 text-gray-600 text-sm'}>
+						Rajdip Bhattacharya is Full Stack developer, Cloud and DevOps engineer based in Kolkata, India.
 					</div>
 					<div className={'flex gap-x-4'}>
-						<a className={'p-2 rounded-full border dark:border-gray-300 border-gray-600'}
-						   href={socials.linkedin}><LinkedIn /></a>
-						<a className={'p-2 rounded-full border dark:border-gray-300 border-gray-600'}
-						   href={socials.github}><GitHub /></a>
-						<a className={'p-2 rounded-full border dark:border-gray-300 border-gray-600'}
-						   href={socials.facebook}><Facebook /></a>
+						<a
+							className={'p-2 rounded-full border dark:border-gray-300 border-gray-600'}
+							href={socials.linkedin}
+						>
+							<LinkedIn />
+						</a>
+						<a
+							className={'p-2 rounded-full border dark:border-gray-300 border-gray-600'}
+							href={socials.twitter}
+						>
+							<Twitter />
+						</a>
+						<a
+							className={'p-2 rounded-full border dark:border-gray-300 border-gray-600'}
+							href={socials.github}
+						>
+							<GitHub />
+						</a>
+						<a
+							className={'p-2 rounded-full border dark:border-gray-300 border-gray-600'}
+							href={socials.facebook}
+						>
+							<Facebook />
+						</a>
 					</div>
 				</div>
 				<div
-					className={'flex flex-col justify-between gap-y-5 dark:text-gray-400 text-gray-600 text-sm md:w-1/5'}>
+					className={
+						'flex flex-col justify-between gap-y-5 dark:text-gray-400 text-gray-600 text-sm md:w-1/5'
+					}
+				>
 					<div>30/17, Shib Chandra Chatterjee Street, Belurmath, Howrah - 711202, India</div>
 					<div>
 						<div>agentR47@gmail.com</div>
@@ -49,8 +75,8 @@ const Footer = () => {
 					</div>
 				</div>
 			</div>
-			<div className={'dark:text-gray-400 text-gray-600 text-center mt-5'}>Made with <span
-				className={'text-red-500 text-lg'}>&#9829;</span> by Rajdip
+			<div className={'dark:text-gray-400 text-gray-600 text-center mt-5'}>
+				Made with <span className={'text-red-500 text-lg'}>&#9829;</span> by Rajdip
 			</div>
 		</AnimationOnScroll>
 	);
